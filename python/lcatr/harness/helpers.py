ad8a6e52b572ebb15dfe987bf652bdf9c0207091
Brett Viren <brett.viren@gmail.com>
2012-12-05 15:38:02 -0500

    Add a helper module for job producers or validators to use.

---
+++ python/lcatr/harness/helpers.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+'''
+This module provides helpful things for producer and validator
+processes, if they be written in Python.
+
+Note, module is not used by the rest of the harness.
+'''
+
+import os
+from glob import glob
+
+def dependency_glob(pattern, jobname = None, paths = None):
+    '''
+    Return list of paths to the files matching glob <pattern> or None.
+
+    If the job name <jobname> is given only consider files in its
+    output area, otherwise only consider file in the output area where
+    the first match is made.  If a list of output <paths> to consider
+    are not given then the value of the environment variable
+    LCATR_DEPENDENCY_PATH is used.
+    '''
+
+    if not paths:
+        paths = os.environ.get('LCATR_DEPENDENCY_PATH')
+    if not paths: return
+    if isinstance(paths, str): 
+        paths = paths.split(':')
+        
+    for path in paths:
+        if jobname and not '/'+jobname+'/' in path: # kind of a cheat
+            continue
+        hit = glob(os.path.join(path,pattern))
+        if hit:
+            return hit
+        continue
+    return
