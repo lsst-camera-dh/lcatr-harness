17ac3d4e6137c70114a544c16a871552296fd475
Brett Viren <bv@bnl.gov>
2012-12-07 10:29:26 -0500

    Add bootstrap ad-hoc installation script.

--- tests/make-fakeinst.sh
+++ tests/make-fakeinst.sh
@@ -3,11 +3,37 @@
 # build out a fake installation that lets one run the code in-place
 # Run this in the directory that holds lcatr/
 
+lcatrurl="https://git.racf.bnl.gov/astro/git/lcatr"
+
 base=$1 ; shift
 if [ -z "$base" ] ; then
     base=$(pwd)/lcatr
 fi
+
+# get the source code
+if [ ! -d $base ] ; then
+    mkdir -p $base/jobs
+    pushd $base
+    for pkg in schema harness modulefiles jobs/example_ana_A jobs/example_station_A jobs/example_station_B
+    do
+	if [ -d $pkg ] ; then
+	    echo "Already have $pkg"
+	    continue
+	fi
+
+	mkdir $pkg
+	pushd $pkg
+	git init
+	git config credential.helper store
+	git remote add central $lcatrurl/${pkg}.git
+	git pull central master
+	popd
+    done
+    popd
+fi
 base=$(readlink -f $base)
+
+# make the install area
 dest=$1 ; shift
 if [ -z "$dest" ] ; then
     dest=fakeinst
@@ -40,4 +66,8 @@ do
     mklink jobs/$job jobs/$job/v0
 done
 
+echo "Source in $base, fake installation in $dest"
+echo "Try running:"
+echo "cd $dest"
+echo "./tests/testrunner test examples"
 
