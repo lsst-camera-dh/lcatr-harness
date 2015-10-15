8129846a451e964bccae23e6086f1bcbec230fbb
Joanne Bogart <jrb@slac.stanford.edu>
2015-10-14 17:38:33 -0700

    allow a retry of remote stat operation

--- python/lcatr/harness/remote.py
+++ python/lcatr/harness/remote.py
@@ -10,21 +10,29 @@ from util import log
 
 ssh_command = "ssh -x"             # rely on it being in PATH
 
-def command(cmdstr):
+def command(cmdstr, retries=1):
     '''
     Run a command return (status,out,err)
     '''
-    log.info('Executing command: %s' % cmdstr)
-    proc = subprocess.Popen(cmdstr, shell=True, 
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out,err = proc.communicate()
-    status = proc.poll()
-    if status == 255: 
-        raise RuntimeError,'SSH cmd "%s" failed.' % (cmdstr)
+    while retries:
+        log.info('Executing command: %s, trials remaining: %s' % (cmdstr, 
+                 str(retries-1)))
+        proc = subprocess.Popen(cmdstr, shell=True, 
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out,err = proc.communicate()
+        status = proc.poll()
+        retries -= 1
+        if status == 255: 
+            if retries == 0:
+                raise RuntimeError,'SSH cmd "%s" failed.' % (cmdstr)
+            else:
+                log.info('SSH cmd "%s" failed. Retrying.' % (cmdstr) )
+        else: retries=0
+
     log.info('Command returned status %d, out="%s" err="%s"' % (status,out,err))
     return status,out,err
 
-def cmd(cmdstr, host = "localhost", user = os.environ.get('USER')):
+def cmd(cmdstr, host = "localhost", user = os.environ.get('USER'), retries = 1):
     '''
     Run cmd on remote as user@host via SSH.
 
@@ -36,7 +44,7 @@ def cmd(cmdstr, host = "localhost", user = os.environ.get('USER')):
     if isinstance(cmdstr,list): cmdstr = ' '.join(cmdstr)
 
     sshcmd = "%s %s@%s %s" % (ssh_command, user, host, cmdstr)
-    return command(sshcmd)
+    return command(sshcmd, retries)
 
 def stat(path, host = "localhost", user = os.environ.get('USER')):
     '''
@@ -44,7 +52,7 @@ def stat(path, host = "localhost", user = os.environ.get('USER')):
 
     See cmd() for return values. 
     '''
-    return cmd("stat %s" % path,host,user)
+    return cmd("stat %s" % path,host,user, retries=2)
 
 
 def rsync(src,dst):
