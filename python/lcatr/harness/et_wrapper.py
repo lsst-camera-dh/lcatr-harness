bae710630bc1149b893b8b2e87d4884db1cda9c8
Joanne Bogart <jrb@slac.stanford.edu>
2016-04-22 12:13:57 -0700

    et_wrapper functions now work

--- python/lcatr/harness/et_wrapper.py
+++ python/lcatr/harness/et_wrapper.py
@@ -9,7 +9,7 @@ from eTraveler.clientAPI.connection import Connection
 import os
 
 def __make_connection():
-    url=os.environ.get('LCATR_LIMSURL')
+    url=os.environ.get('LCATR_LIMS_URL')
     operator=os.environ.get('LCATR_OPERATOR')
     # Need to parse out experiment, db and whether it's prod or dev server
     # Assume defaults to start
@@ -21,11 +21,18 @@ def __make_connection():
     if 'exp' in cmps:
         expIx = cmps.index('exp') + 1
         exp = cmps[expIx]
-    db = cmps[cmps.index('Results') - 1]
+    if 'Results' in cmps:
+        db = cmps[cmps.index('Results') - 1]
+    else:
+        if 'eTraveler' in cmps:
+            db = cmps[cmps.index('eTraveler') + 1]
+        else:
+            return None
+    
     return Connection(operator, db, prodServer)
 
 
-def setHardwareStatus(newStatus):
+def setHardwareStatus(newStatus, reason='From harnessed job'):
     '''
     Set hardware status of component job is concerned with.
     newStatus:  String value which must correspond to legitimate hardware
@@ -33,13 +40,16 @@ def setHardwareStatus(newStatus):
     returns: String value. 'Success' if operation succeeded; else error message
     '''
     conn = __make_connection()
-    experimentSN=os.environ.get('LCATR_UNIT_ID')
-    htype=os.environ.get('LCATR_UNIT_TYPE')
-    msg = conn.setHardwareStatus(experimentSN=experimentSN, htype=htype,
-                                 attributeName=newStatus)
+    if conn == None: return 'Unable to connect to eTraveler server'
+    expSN=os.environ.get('LCATR_UNIT_ID')
+    ht=os.environ.get('LCATR_UNIT_TYPE')
+    actId = os.environ.get('LCATR_JOB_ID')
+    msg = conn.setHardwareStatus(experimentSN=expSN, htype=ht,
+                                 status=newStatus, 
+                                 reason=reason, activityId=actId)
     return msg
 
-def adjustHardwareLabel(label, add=True):
+def adjustHardwareLabel(label, add=True, reason='from harnessed job'):
     '''
     Add a label to or remove from component job is concerned with.
     label:   String value of label to be added or removed.  Must correspond
@@ -48,12 +58,15 @@ def adjustHardwareLabel(label, add=True):
     returns: String value. 'Success' if operation succeeded, else error message
     '''
     conn = __make_connection()
+    if conn == None: return 'Unable to connect to eTraveler server'
     experimentSN=os.environ.get('LCATR_UNIT_ID')
     htype=os.environ.get('LCATR_UNIT_TYPE')
     if add: adding='true'
     else: adding='false'
+    actId = os.environ.get('LCATR_JOB_ID')
     msg = conn.adjustHardwareLabel(experimentSN=experimentSN, htype=htype,
-                                  attributeName=label, adding=adding)
+                                   label=label, adding=adding,
+                                   reason=reason, activityId=actId)
     return msg
                                    
 
